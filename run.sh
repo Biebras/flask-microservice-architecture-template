12ee99ec24197c3280017a2d6f33bac85acc2250
sc21sv <sauliusvince@gmail.com>
2023-04-29 14:20:19 +0100

    Reorganising template

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t main-service -f ./services/main_service/Dockerfile ./main_service
-docker build -t service-1 -f ./services/service_1/Dockerfile ./service_1
-docker build -t service-2 -f ./services/service_2/Dockerfile ./service_2
+docker build -t main-service -f ./services/main_service/Dockerfile ./services/main_service
+docker build -t service-1 -f ./services/service_1/Dockerfile ./services/service_1
+docker build -t service-2 -f ./services/service_2/Dockerfile ./services/service_2
 
 docker-compose down
 docker-compose up
--- services/main_service/app/views.py
+++ services/main_service/app/views.py
@@ -15,6 +15,12 @@ def get_service_2():
     response = requests.get('http://service-2:8002/service_2/api_1')
     return response.json()
 
+@app.route('/post_service_1')
+def post_service_1():
+    data = {"key": "value"}
+    response = requests.post('http://service-1:8001/service_1/api_2', json=data)
+    return response.json()
+
 @app.route('/post_service_2')
 def post_service_2():
     data = {"key": "value"}
