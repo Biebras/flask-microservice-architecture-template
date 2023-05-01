7ea9031ace5bbf8d5c5da4e3ac95a37384f98967
sc21sv <sauliusvince@gmail.com>
2023-05-01 01:36:34 +0100

    Merge test_main and test into 1 file

--- test.sh
+++ test.sh
@@ -2,3 +2,11 @@
 
 pytest services/service_1/
 pytest services/service_2/
+
+docker build -t main-service -f ./services/main_service/Dockerfile ./services/main_service
+docker build -t service-1 -f ./services/service_1/Dockerfile ./services/service_1
+docker build -t service-2 -f ./services/service_2/Dockerfile ./services/service_2
+
+docker-compose -f docker-compose.test.yml up -d
+docker-compose logs --tail=1000 -f main-service
+docker-compose down
--- test_main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t main-service -f ./services/main_service/Dockerfile ./services/main_service
-docker build -t service-1 -f ./services/service_1/Dockerfile ./services/service_1
-docker build -t service-2 -f ./services/service_2/Dockerfile ./services/service_2
-
-docker-compose -f docker-compose.test.yml up -d
-docker-compose logs --tail=1000 -f main-service
-docker-compose down
